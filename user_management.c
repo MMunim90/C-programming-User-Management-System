eec54abad1d27d7ae2eafaf08ff02b08edae9b5f
MMunim <mmunim9.9.01@gmail.com>
2026-08-09 09:06:05 +0600

    complete basic part of the problem

--- user_management.c
+++ user_management.c
@@ -1,6 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void register_user();
+void login_user();
+
 int main()
 {
-    printf("Hello c!");
+    while (1)
+    {
+        int option;
+        printf("\n ----- Welcome to User Management System -----");
+        printf("\n1. Register");
+        printf("\n2. Login");
+        printf("\n3. Exit");
+
+        printf("\nSelect an option: ");
+        scanf("%d", &option);
+        
+        if(option == 1){
+            // 
+        }
+        else if(option == 2){
+            // 
+        }
+        else if(option == 3){
+            printf("\nExiting Program...");
+            return 0;
+        }
+        else{
+            printf("\nInvalid Option. Please Try Again!");
+        }
+    }
+
     return 0;
 }
