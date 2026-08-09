b3d09bd83b41dd5b07574fe22919b7077fa9eabe
MMunim <mmunim9.9.01@gmail.com>
2026-08-09 10:13:13 +0600

    complete main function part

--- user_management.c
+++ user_management.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#define MAX_USERS 10
+
+typedef struct
+{
+    char username[30];
+    char password[30];
+} User;
+
+User users[MAX_USERS];
+int user_count = 0;
 
 void register_user();
-void login_user();
+int login_user(); // returns the user index
 
 int main()
 {
@@ -17,19 +27,33 @@ int main()
         scanf("%d", &option);
         
         if(option == 1){
-            // 
+            register_user(); 
         }
         else if(option == 2){
-            // 
+            int user_index = login_user(); 
+
+            if(user_index >= 0){
+                printf("\nLogin successful! Welcome, %s!", users[user_index].username);
+            }else{
+                printf("\nLogin failed! Incorrect username or password.\n");
+            }
         }
         else if(option == 3){
-            printf("\nExiting Program...");
+            printf("\nExiting Program.\n");
             return 0;
         }
         else{
-            printf("\nInvalid Option. Please Try Again!");
+            printf("\nInvalid Option. Please Try Again!\n");
         }
     }
 
     return 0;
+}
+
+void register_user(){
+    printf("Dummy registration");
+}
+
+int login_user(){
+    return -1;
 }
